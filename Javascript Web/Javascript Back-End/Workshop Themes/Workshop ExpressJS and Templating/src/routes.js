11bce4d76f52d68963855e8fa8ac6acd4fa3954d
Jykata <jykatabg@gmail.com>
2023-02-04 19:05:07 +0200

    add login and register pages

--- Javascript Web/Javascript Back-End/Workshop Themes/Workshop ExpressJS and Templating/src/routes.js
+++ Javascript Web/Javascript Back-End/Workshop Themes/Workshop ExpressJS and Templating/src/routes.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 const cubeController = require('./controllers/cubeController.js');
 const homeController = require('./controllers/homeController.js');
 const accessoryController = require('./controllers/accessoryController.js');
+const authController = require('./controllers/authController.js');
 
 //All homeControllers
 router.get('/', homeController.getHomePage);
@@ -14,10 +15,13 @@ router.get('/cubes/create', cubeController.getCreateCube);
 router.post('/cubes/create', cubeController.postCreateCube);
 router.get('/cubes/:cubeId/details', cubeController.detailsCreateCube);
 
+//Attach
 router.get('/cubes/:cubeId/attach', cubeController.getAttachAccessory);
 router.post('/cubes/:cubeId/attach', cubeController.postAttachAccessory);
-
+//Accessories
 router.use('/accessories', accessoryController);
 
+//Authentication
+router.use('/', authController);
 
 module.exports = router;
