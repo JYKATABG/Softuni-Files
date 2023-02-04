28312b7455583b9cd860f8dec660f3d274a82a66
Jykata <jykatabg@gmail.com>
2023-02-04 23:07:28 +0200

    sent cookie to client

--- Javascript Web/Javascript Back-End/Workshop Themes/Workshop ExpressJS and Templating/src/controllers/authController.js
+++ Javascript Web/Javascript Back-End/Workshop Themes/Workshop ExpressJS and Templating/src/controllers/authController.js
@@ -11,7 +11,7 @@ router.post('/login', async (req, res) => {
 
     try {
         const token = await authManager.login(username, password);
-        console.log(token);
+        res.cookie('auth', token, { httpOnly: true })
     } catch (err) {
         console.log(err);
 
