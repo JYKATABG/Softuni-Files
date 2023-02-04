0e828155902e2eaf19e0ae7c7987aefd65152ddb
Jykata <jykatabg@gmail.com>
2023-02-04 21:29:07 +0200

    add register

--- Javascript Web/Javascript Back-End/Workshop Themes/Workshop ExpressJS and Templating/src/controllers/authController.js
+++ Javascript Web/Javascript Back-End/Workshop Themes/Workshop ExpressJS and Templating/src/controllers/authController.js
@@ -1,11 +1,32 @@
 const router = require('express').Router();
 
+const authManager = require('../manager/authManager.js');
+
 router.get('/login', (req, res) => {
     res.render('auth/login');
 });
 
-router.get('/register', (req, res) => {
+router.get('/register', async (req, res) => {
     res.render('auth/register');
+});
+
+router.post('/register', async (req, res) => {
+
+    const { username, password, repeatPassword } = req.body;
+
+    if (password !== repeatPassword) {
+        return res.status(404).end();
+    }
+
+    const existingUser = await authManager.getUserByUsername(username);
+
+    if (existingUser) {
+        return res.status(404).end();
+    }
+
+    const user = authManager.register(username, password);
+
+    res.redirect('/login');
 });
 
 
