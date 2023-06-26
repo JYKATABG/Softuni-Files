01219441bcfbd93888ad6a5d1c8354aee31c3189
Jykata <jykatabg@gmail.com>
2023-06-26 17:49:11 +0300

    Todo exercise with Angular
    Features added:
    - Available design and structure
    - Create todo
    - Check todo
    - Delete todo

--- Javascript Front-End/Angular/Components/components-demo/src/app/app.module.ts
+++ Javascript Front-End/Angular/Components/components-demo/src/app/app.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TodoAppComponent } from './todo-app/todo-app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, TodoAppComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
