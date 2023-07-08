2d54d7f854d18828c67a9309804751e93ed95c09
Jykata <jykatabg@gmail.com>
2023-07-09 02:02:05 +0300

    Modules and Routing Workshop forum-app part2
    - Routing
    - Fake Auth (localstorage)
    - Several Pages
    - Dynamic templates

---
+++ Javascript Front-End/Angular/Workshop Modules and Routing/forum-app advanced/src/app/api.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment.development';
+import { Theme } from './types/Theme';
+import { Post } from './types/Post';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  constructor(private http: HttpClient) {}
+
+  //Http data requests
+  getPosts(limit?: number) {
+    const limitFilter = limit ? `?limit=${limit}` : '';
+
+    return this.http.get<Post[]>(`${environment.appUrl}/posts${limitFilter}`);
+  }
+  getThemes() {
+    return this.http.get<Theme[]>(`${environment.appUrl}/themes`);
+  }
+
+  getOneTheme(id: string) {
+    return this.http.get<Theme>(`${environment.appUrl}/themes/${id}`);
+  }
+}
